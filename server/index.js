5434b6b26b91aa873bad3b0783031674491549d0
Taras Galatsiuk <galatsiuk.taras@gmail.com>
2019-08-31 15:27:03 +0300

    reinstalled express --save

--- server/index.js
+++ server/index.js
@@ -21,6 +21,7 @@ if(process.env.NODE_ENV == 'production') {
     app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
 
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
